a70dcd026cb4cc2fa9fb51146fac6e08c4369ac2
Warner Vance <warnervance@icloud.com>
2024-12-21 22:56:10 -0600

    Added way to get all interview for a specific pledge

--- interviews.py
+++ interviews.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import functions as fn
-from logging import getLogger
 from logging_config import setup_logging
+
 logger = setup_logging()
 
 def add_interview(pledge, brother, quality, time):
@@ -13,7 +13,7 @@ def add_interview(pledge, brother, quality, time):
         return 1
     try:
         df = pd.read_csv('interviews.csv')
-    except:
+    except Exception:
        logger.error('error reading interviews.csv')
        return 1 
     try:
@@ -22,6 +22,17 @@ def add_interview(pledge, brother, quality, time):
         added_interview = [time, pledge, brother, quality]
         df.loc[len(df)] = added_interview
         return 0
-    except:
+    except Exception:
         logger.error('Error adding interview')
+        return 1
+def get_pledge_interviews(pledge):
+    """
+    Gets a dataframe of pledge interviews for a specific pledge
+    :param pledge: Name of Pledge to check
+    :return: pandas dataframe of pledge interviews
+    """
+    if fn.check_pledge(pledge):
+        df = pd.read_csv('interviews.csv')
+        return df.loc[df['pledge'] == pledge]
+    else:
         return 1
